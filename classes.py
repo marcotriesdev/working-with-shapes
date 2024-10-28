08937447c1b79a78ca716c43a34e1803d5198207
marcotriesdev <50000tons@gmail.com>
2024-10-28 13:40:17 -0600

    SE CREO LA STATE MACHINE Y SE ENCAPSULO TODO EN LOOPS, AHORA YA HAY MENU TITULO, MAIN GAME Y PANTALLA DE MUERTO

--- classes.py
+++ classes.py
@@ -1,4 +1,55 @@
 from raylibpy import *
+from enum import Enum
+
+class Estado_global(Enum):
+
+    intro = 1
+    main_game = 2
+    pause = 3
+    dead = 4
+
+
+class Estado(Enum):
+
+    idle = 1 
+    chasing = 2
+    attack = 3
+    flee = 4
+
+class SimpleStateMachine:
+
+    def __init__(self, init_state,WINDOW_WIDTH,WINDOW_HEIGHT,gametitle):
+        self.init_state = init_state
+        self.current_state = self.init_state
+        self.dimensions = Vector2(WINDOW_WIDTH,WINDOW_HEIGHT)
+        self.gametitle = gametitle
+        
+
+    def update(self):
+
+        while not window_should_close():
+            print("xulo")
+
+            match self.current_state:
+
+                case Estado_global.intro:
+                    print(self.current_state)
+                    self.current_state = intro_loop(self.dimensions.x,self.dimensions.y,self.gametitle,self.current_state)
+
+                case Estado_global.main_game:
+
+                    print(self.current_state)
+                    print("EMPEZAMOS juego MALDITOS")
+                    self.current_state = game_loop(self.dimensions.x, self.dimensions.y,self.gametitle,self.current_state)
+                #case Estado_global.pause
+                case Estado_global.dead:
+                    print(self.current_state)
+                    self.current_state = dead_loop(self.dimensions.x, self.dimensions.y,self.gametitle,self.current_state)
+
+
+
+
+        close_window()
 
 class Circle:
 
@@ -159,12 +210,12 @@ class Group:
 
 class Gui:
 
-    def __init__(self, window_width, window_height,title,player):
+    def __init__(self, window_width, window_height,title,player = None):
         
         self.window_width = window_width
         self.window_height = window_height
         self.title = title
-        self.titleposition = (window_width/2)-(25*(len(self.title)/2))
+        self.titlepositionx = (window_width/2)-(25*(len(self.title)/2))
         self.player = player
 
         pass
@@ -173,10 +224,116 @@ class Gui:
     def draw_hud(self):
 
         #DRAW GAME TITLE
-        draw_text(self.title, self.titleposition, 5, 40, BLACK)
+        draw_text(self.title, self.titlepositionx, 5, 40, BLACK)
 
         #DRAW SCORE
         draw_text(f"SCORE: {self.player.score}",50,50,40,BLACK)
 
         #DRAW HP
         draw_text(f"HP: {self.player.hp}", 50,120,40,BLACK)
+
+    def draw_intro(self):
+
+        draw_text(self.title, self.titlepositionx,self.window_height/2,30,BLACK)
+        draw_text("//press ENTER//", self.titlepositionx+20,(self.window_height/2)+50,30,RED)
+
+    def draw_dead(self):
+        draw_text("//TE MORISTE JAJAJAJAJA//", self.titlepositionx+20,(self.window_height/2)+50,30,RED)
+        draw_text("PRESIONA ENTER PARA REVIVIR", self.titlepositionx+20,(self.window_height/2)+80,30,GREEN)
+
+
+def intro_loop(WINDOW_WIDTH,WINDOW_HEIGHT,gametitle,current_state):
+
+    introGUI = Gui(WINDOW_WIDTH,WINDOW_HEIGHT,gametitle)
+
+    while not window_should_close():
+
+        begin_drawing()
+
+        clear_background(WHITE)
+
+        introGUI.draw_intro()
+
+        if is_key_pressed(KEY_ENTER):
+
+            current_state = Estado_global.main_game
+            print(f"orale me voy a {current_state}")
+            return current_state
+
+        end_drawing()
+    
+    close_window()
+
+
+
+def game_loop(WINDOW_WIDTH,WINDOW_HEIGHT,game_title,current_state):
+
+    
+    dimensions = Vector2(WINDOW_WIDTH,WINDOW_HEIGHT)
+                    
+    circle1 = Circle(Vector2(500,500),10,PINK,"fill",10,5)
+    gamegui = Gui(WINDOW_WIDTH,WINDOW_HEIGHT, game_title,circle1)
+
+    circle2 = Circle(Vector2(400,400),15,GREEN,"outline",1)
+    circle3 = Circle(Vector2(460,400),15,GREEN,"outline",1)
+    circle4 = Circle(Vector2(520,400),15,GREEN,"outline",1)
+
+    enemy1 = Circle(Vector2(800,800),20,BLACK,"enemy",10,4,2)
+
+    player_group = Group([circle1])
+    circle_group = Group([circle2,circle3,circle4])
+    circle_group.set_group_thickness(10)
+
+    enemy_group = Group([enemy1])
+
+
+    while not window_should_close():
+
+        begin_drawing()
+
+        clear_background(WHITE)
+
+        player_group.update(dimensions)
+        if circle1.hp <= 0:
+            current_state = Estado_global.dead
+            print(f"orale me voy a {current_state}")
+            return current_state
+
+        enemy_group.update(dimensions)
+        circle_group.update(dimensions)
+
+        circle1.collision(circle_group)
+        circle1.collision(enemy_group)
+
+        player_group.draw()
+        circle_group.draw()
+        enemy_group.draw()
+        gamegui.draw_hud()
+
+        end_drawing()
+        
+    close_window()
+    
+
+
+def dead_loop(WINDOW_WIDTH,WINDOW_HEIGHT,gametitle,current_state):
+
+    deadGUI = Gui(WINDOW_WIDTH,WINDOW_HEIGHT,gametitle)
+
+    while not window_should_close():
+
+        begin_drawing()
+
+        clear_background(WHITE)
+
+        deadGUI.draw_dead()
+
+        if is_key_pressed(KEY_ENTER):
+
+            current_state = Estado_global.main_game
+            print(f"orale me voy a {current_state}")
+            return current_state
+
+        end_drawing()
+    
+    close_window()
--- shapes.py
+++ shapes.py
@@ -11,45 +11,73 @@ set_trace_log_level(LOG_NONE)
 
 set_target_fps(60)
 
+#--------------------------------------------------------
+
+estado_global = Estado_global.intro
+
+state_machine = SimpleStateMachine(estado_global,dimensions.x,
+                                   dimensions.y,game_title)
+
+#--------------------------------------------------------
 
 
-circle1 = Circle(Vector2(500,500),10,PINK,"fill",10,5)
-gamegui = Gui(WINDOW_WIDTH,WINDOW_HEIGHT, game_title,circle1)
 
-circle2 = Circle(Vector2(400,400),15,GREEN,"outline",1)
-circle3 = Circle(Vector2(460,400),15,GREEN,"outline",1)
-circle4 = Circle(Vector2(520,400),15,GREEN,"outline",1)
-
-enemy1 = Circle(Vector2(800,800),20,BLACK,"enemy",10,4,2)
-
-player_group = Group([circle1])
-circle_group = Group([circle2,circle3,circle4])
-circle_group.set_group_thickness(10)
-
-enemy_group = Group([enemy1])
+state_machine.update()
 
 
-while not window_should_close():
 
-    begin_drawing()
 
-    clear_background(WHITE)
 
-    player_group.update(dimensions)
-    enemy_group.update(dimensions)
-    circle_group.update(dimensions)
 
-    circle1.collision(circle_group)
-    circle1.collision(enemy_group)
 
-    player_group.draw()
-    circle_group.draw()
-    enemy_group.draw()
-    gamegui.draw_hud()
 
-    end_drawing()
-    
-close_window()
+
+
+
+
+
+
+
+
+def sacar():
+        
+    circle1 = Circle(Vector2(500,500),10,PINK,"fill",10,5)
+    gamegui = Gui(WINDOW_WIDTH,WINDOW_HEIGHT, game_title,circle1)
+
+    circle2 = Circle(Vector2(400,400),15,GREEN,"outline",1)
+    circle3 = Circle(Vector2(460,400),15,GREEN,"outline",1)
+    circle4 = Circle(Vector2(520,400),15,GREEN,"outline",1)
+
+    enemy1 = Circle(Vector2(800,800),20,BLACK,"enemy",10,4,2)
+
+    player_group = Group([circle1])
+    circle_group = Group([circle2,circle3,circle4])
+    circle_group.set_group_thickness(10)
+
+    enemy_group = Group([enemy1])
+
+
+    while not window_should_close():
+
+        begin_drawing()
+
+        clear_background(WHITE)
+
+        player_group.update(dimensions)
+        enemy_group.update(dimensions)
+        circle_group.update(dimensions)
+
+        circle1.collision(circle_group)
+        circle1.collision(enemy_group)
+
+        player_group.draw()
+        circle_group.draw()
+        enemy_group.draw()
+        gamegui.draw_hud()
+
+        end_drawing()
+        
+    close_window()
 
 
 
